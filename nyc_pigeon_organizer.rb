a6f3561ac05b13339ea82af64bd000b94ae76e59
Ming Duan <11203duan@gmail.com>
2019-03-06 02:37:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,16 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
 
   pigeon_list = {}
 
   data.each do
 
+=======
+  pigeon_list = {}
+  data.each do
+>>>>>>> 375370171f100d94be8cb8a6731b88234230a672
     |category1, detail1| detail1.each do
 
           |attrs, name| name.each do
@@ -13,6 +18,7 @@ def nyc_pigeon_organizer(data)
             |each_name|
 
             if pigeon_list[each_name].nil?
+<<<<<<< HEAD
 
             pigeon_list[each_name] = {}
 
@@ -20,6 +26,12 @@ def nyc_pigeon_organizer(data)
             pigeon_list[each_name][category1] << attrs.to_s
 
 
+=======
+            pigeon_list[each_name] = {}
+            pigeon_list[each_name][category1] = []
+            pigeon_list[each_name][category1] << attrs.to_s
+            binding.pry
+>>>>>>> 375370171f100d94be8cb8a6731b88234230a672
           elsif pigeon_list[each_name][category1].nil?
             pigeon_list[each_name][category1] = []
             pigeon_list[each_name][category1] << attrs.to_s
@@ -31,5 +43,8 @@ def nyc_pigeon_organizer(data)
         end
       end
     end
+<<<<<<< HEAD
     pigeon_list
+=======
+>>>>>>> 375370171f100d94be8cb8a6731b88234230a672
     end
