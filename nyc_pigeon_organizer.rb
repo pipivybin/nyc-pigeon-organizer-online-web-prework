78e0efc7a7403e5f99af2156a3cca429e3d48610
Ming Duan <11203duan@gmail.com>
2019-03-06 02:01:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+
+  pigeon_list = {}
+
+  data.each do
+
+    |category1, detail1| detail1.each do
+
+          |attrs, name| name.each do
+
+            |each_name|
+
+            if pigeon_list[each_name].nil?
+
+            pigeon_list[each_name] = {}
+
+            pigeon_list[each_name][category1] = []
+            pigeon_list[each_name][category1] << attrs.to_s
+
+
+          elsif pigeon_list[each_name][category1].nil?
+            pigeon_list[each_name][category1] = []
+            pigeon_list[each_name][category1] << attrs.to_s
+
+          else
+            pigeon_list[each_name][category1] << attrs.to_s
+            end
+
+        end
+      end
+    end
+    pigeon_list
+    end
